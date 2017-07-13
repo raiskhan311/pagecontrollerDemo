748d520abaec933e8a5816bc2991d09875164be7
mac <mac@macs-iMac.local>
2017-07-13 18:26:44 +0530

    page controller demo Auto change picture

--- PageControllerDemo/ViewController.swift
+++ PageControllerDemo/ViewController.swift
@@ -9,17 +9,53 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var img_view: UIImageView!
+    
+    @IBOutlet weak var pge_control: UIPageControl!
+    
+    var timer: Timer!
+    var updatecounter: Int!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        updatecounter = 0
+        timer = Timer.scheduledTimer(timeInterval: 1.5, target: self, selector: #selector(ViewController.updateTimer), userInfo: nil, repeats: true)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    internal func updateTimer()
+    {
+        if (updatecounter <= 4)
+        {
+            pge_control.currentPage = updatecounter
+            img_view.image = UIImage(named: String(updatecounter) + ".jpg")
+            updatecounter = updatecounter + 1
+            print(updatecounter)
+        }
+        else
+        {
+            updatecounter = 0
+        }
     }
-
-
+    
+    @IBAction func btn_pageController(_ sender: Any) {
+        updatedImage()
+        print(updatecounter)
+    }
+    
+    func updatedImage()
+    {
+        if updatecounter <= 4
+        {
+            img_view.image = UIImage(named: String(updatecounter) + ".jpg")
+            updatecounter = updatecounter + 1
+        }
+        else
+        {
+            updatecounter = 0
+        }
+    }
+    
 }
 
